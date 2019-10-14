dc0c368458bbff933a14ffd2f222cc7b900e00d4
Vadim Manaenko <vadim.razorq@gmail.com>
2019-10-14 14:59:44 +0300

    Fix start/finish liability

--- fetchNixpkgs.nix
+++ fetchNixpkgs.nix
@@ -1,5 +1,5 @@
 { rev    ? "b8abf91a11ad0e5f8492f3c6ec66f18d57c26e92"             # The Git revision of nixpkgs to fetch
-, sha256 ? "08wpjki78rbn4ncv550xxsb1pxlsgln9nr8qw4ilb7lkicy8jk1d" # The SHA256 of the downloaded data
+, sha256 ? "09wpjki78rbn4ncv550xxsb1pxlsgln9nr8qw4ilb7lkicy8jk1d" # The SHA256 of the downloaded data
 }:
 
 builtins.fetchTarball {
